4f2cbcf26aecf5027e783f5644b5470da620d08f
slo2d152021 <83452333+marcinMslo@users.noreply.github.com>
2021-05-25 13:45:33 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,55 @@
+function z1()
+{
+     document.getElementById("z1").innerHTML="Było 6, niosło 3 i dwóch kolejnych niosło kajak i wiosło";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Tu znajdziesz odpowiedź";
+    } 
+
+
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="Nie padało.";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Tu znajdziesz odpowiedź";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="Tylko jeden, ten zabity. Pozostałe oczywiście uciekły.";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="4 koty.";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Jedna bogini dostała jabłko z koszykiem.";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Tu znajdziesz odpowiedź";
+    }
